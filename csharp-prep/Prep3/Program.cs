c7853d389395ae37223666fcdcb1ef9b4b539345
Eric Shewchuk <eric@thebanyanteam.com>
2023-04-18 09:30:35 -1000

    completed core and stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "";
+        int guess = 0;
+        bool play = true;
+        string playAgain = "";
+
+        while (play)
+        {   
+        int tries = 0;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+            do
+            {
+                Console.Write("What is your guess? ");
+                response = Console.ReadLine();
+                guess = int.Parse(response);
+                tries ++;
+                if (guess < number)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too High");
+                } 
+            } while (guess != number);
+            
+            Console.WriteLine($"Congratulations!  You got it in {tries} tries.");
+            Console.Write("Would you like to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+            if (playAgain == "no")
+            {
+                play = false;
+            }
+        }
     }
 }
