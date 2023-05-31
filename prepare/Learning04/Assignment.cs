6097c0fa8ab3bae1c41c1b41d256e6dd70fb6fd5
Eric Shewchuk <eric@thebanyanteam.com>
2023-05-30 14:53:08 -1000

    added a comment

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -13,4 +13,10 @@ public class Assignment
     {
         return ($"{_studentName} - {_topic}");
     }
+
+    /* After completing the assignment I viewed the sample exercise.  I see that it uses
+       getter and setter methods to make the attributes here accessible to outside code.
+       I believe I understand how that works and why it would be useful in many cases.  
+       Since I was able to complete the assignment with proper output without using those 
+       methods, I have decided not to update my code to be more like the example. */
 }
