8c6d990fc831042a77cc6ab761b568165285aa46
Eric Shewchuk <eric@thebanyanteam.com>
2023-06-14 14:32:06 -1000

    wrote the learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>{};
+        Shape block = new Square("blue", 4);
+        shapes.Add(block);
+        Shape block2 = new Rectangle("green", 4, 10);
+        shapes.Add(block2);
+        Shape sphere = new Circle("red", 4);
+        shapes.Add(sphere);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The color is {shape.GetColor()} and the area is {shape.GetArea()}");
+        }
     }
 }
